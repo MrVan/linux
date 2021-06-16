a6a7bbfd01cdea6bbb5f88a896bfddfcbee240b1
Marek Vasut <marex@denx.de>
2021-06-16 13:09:10 +0200

    Revert "drm/exynos: use pm_runtime_resume_and_get()"
    This reverts commit 445d3bed75de4082c7c7794030ac9a5b8bfde886.

--- drivers/gpu/drm/exynos/exynos7_drm_decon.c
+++ drivers/gpu/drm/exynos/exynos7_drm_decon.c
@@ -531,16 +531,11 @@ static void decon_init(struct decon_context *ctx)
 static void decon_atomic_enable(struct exynos_drm_crtc *crtc)
 {
 	struct decon_context *ctx = crtc->ctx;
-	int ret;
 
 	if (!ctx->suspended)
 		return;
 
-	ret = pm_runtime_resume_and_get(ctx->dev);
-	if (ret < 0) {
-		DRM_DEV_ERROR(ctx->dev, "failed to enable DECON device.\n");
-		return;
-	}
+	pm_runtime_get_sync(ctx->dev);
 
 	decon_init(ctx);
 
